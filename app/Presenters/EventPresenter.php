629784494c9f0dc083ef3ab8e82af2f3c2d961df
Jeffrey Davidson <jdavidsonwebdev@gmail.com>
2022-01-07 19:53:18 -0500

    wip

--- app/Presenters/EventPresenter.php
+++ app/Presenters/EventPresenter.php
@@ -2,15 +2,22 @@
 
 namespace App\Presenters;
 
+use App\Models\Event;
+
 class EventPresenter extends Presenter
 {
     /**
+     * @var Event $model
+     */
+    protected $model;
+
+    /**
      * Retrieve the formatted event date.
      *
-     * @return string
+     * @return ?string
      */
     public function date()
     {
-        return $this->model->date->format('F j, Y');
+        return $this->model->date?->format('F j, Y');
     }
 }
